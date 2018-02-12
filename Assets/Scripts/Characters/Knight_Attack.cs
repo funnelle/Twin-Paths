fe08df0e66ea8f374c21c6e0f3a215ee4e48508e
NicolasP <nicolasDpowers@gmail.com>
2018-02-11 20:07:16 -0700

    Added Placeholder Knight + PH Anims + Hit Detection. Note: Hit Detection doesn't work (at bottom of control scripts) and knight attack bool will not change to false

---
+++ Assets/Scripts/Characters/Knight_Attack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight_Attack : MonoBehaviour {
+	BoxCollider2D sword;
+	public float attackTime;
+
+	void Awake() {
+		sword = GetComponent<BoxCollider2D> ();
+	}
+
+	public void KnightAttack() {
+		sword.enabled = true;
+		StartCoroutine (AttackLength (attackTime));
+	}
+
+	private IEnumerator AttackLength (float attackTime) {
+		yield return new WaitForSeconds (attackTime);
+		sword.enabled = false;
+	}
+}
