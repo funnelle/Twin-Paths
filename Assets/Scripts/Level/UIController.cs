93e65bdacf6cfd151a87106538e630bfc81b212b
LtGecko <nicolasDpowers@gmail.com>
2018-04-01 12:19:16 -0600

    Renamed "VictoryScreen" UI to "UIOverlay". Still nonfunctional

---
+++ Assets/Scripts/Level/UIController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+
+	// Button Variables
+	public string menuButton = "Menu";
+	
+	// Components
+	private GameObject UIOverlay;
+	
+	// Use this for initialization
+	void Start () {
+		
+		UIOverlay = GameObject.Find("UIOverlay");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
