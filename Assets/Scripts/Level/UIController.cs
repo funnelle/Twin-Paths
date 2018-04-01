9009a999ce827be4871404deb5fb6f6d1f19835a
LtGecko <nicolasDpowers@gmail.com>
2018-04-01 16:12:19 -0600

    Game loop set. Starting from main menu can lead to a win state whereby the game loops back to the main menu. Music does not start and stop between intermediary levels, only between edge and intermediary levels.

--- Assets/Scripts/Level/UIController.cs
+++ Assets/Scripts/Level/UIController.cs
@@ -1,12 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditorInternal.VersionControl;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour {
 
+	// Integers
+	public int Delay = 3;
+	public int SceneIndex = 0;
+	
 	// Button Variables
 	public string menuButton = "Menu";
 	
@@ -14,12 +20,20 @@ public class UIController : MonoBehaviour {
 	public GameObject UIOverlay;
 	public GameObject PauseScreen;
 	public GameObject VictoryScreen;
+	public GameObject Knight;
+	public GameObject Rogue;
 	
 	// Bools
 	private bool isPauseShowing;
-	public bool KnightWin;
-	public bool RogueWin;
-	
+	private bool KnightWin;
+	private bool RogueWin;
+
+	private void Awake()
+	{
+		Knight = GameObject.Find("KnightPlayer");
+		Rogue = GameObject.Find("RoguePlayer");
+	}
+
 	// Use this for initialization
 	void Start ()
 	{
@@ -28,19 +42,53 @@ public class UIController : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
-
-		KnightWin = GameObject.Find("LevelChanger").GetComponent<RKLevelController>().KnightWin;
-		RogueWin = GameObject.Find("LevelChanger").GetComponent<RKLevelController>().RogueWin;
+		if (SceneManager.GetActiveScene().name == "Throne_Room" || SceneManager.GetActiveScene().name == "Rogue_Hideout")
+		{
+			KnightWin = GameObject.Find("LevelChanger").GetComponent<RKLevelController>().KnightWin;
+			RogueWin = GameObject.Find("LevelChanger").GetComponent<RKLevelController>().RogueWin;
+		}
 
 		if (Input.GetButtonDown(menuButton))
 		{
 			isPauseShowing = !isPauseShowing;
 			PauseScreen.SetActive(isPauseShowing);
+			Knight.GetComponent<KnightController>().allowedMovement = false;
+			Rogue.GetComponent<RogueController>().allowedMovement = false;
+		}
+
+		if (!isPauseShowing)
+		{
+			if (Knight != null)
+			{
+				Knight.GetComponent<KnightController>().allowedMovement = true;
+			}
+
+			if (Rogue != null)
+			{
+				Rogue.GetComponent<RogueController>().allowedMovement = true;
+			}
 		}
 
 		if (KnightWin || RogueWin)
 		{
-			VictoryScreen.SetActive(KnightWin || RogueWin);
+			VictoryScreen.SetActive(true);
+
+			if (Knight != null)
+			{
+				Knight.GetComponent<KnightController>().allowedMovement = false;
+
+			}
+			if (Rogue != null)
+			{
+				Rogue.GetComponent<RogueController>().allowedMovement = false;
+			}
+			
+			Invoke("BackToMenu", Delay);
 		}
+	}
+
+	void BackToMenu()
+	{
+		SceneManager.LoadScene(SceneIndex);
 	}
 }
---
+++ Assets/Scripts/MusicChanger.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.iOS;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicChanger : MonoBehaviour {
+
+	private List<string> _rkScenesList;
+	
+	private Scene _currentScene;
+	private string _currentSceneName;
+
+	public GameObject BGM;
+	public AudioSource _musicManager;
+
+	private GameObject FBGM;
+	private AudioSource _finalMusic;
+
+	//private AudioClip _currentAudioClip;
+	//public AudioClip BattleTheme;
+	//public AudioClip FinalBattleTheme;
+
+	private static bool AudioBegin = false;
+	
+	// On Awake
+	void Awake()
+	{
+		// List will contain 5 scene names when game is completed -> REMEMBER TO ADD AS LISTED, ORDER MATTERS
+		// Reference list of levels:
+		// "Rogue_Hideout"
+		// "Tavern"
+		// "Courtyard"
+		// "Bangin' Banners"
+		// "Throne_Room"
+
+		_currentScene = SceneManager.GetActiveScene();
+		_currentSceneName = _currentScene.name;
+
+		_musicManager = GetComponent<AudioSource>();
+		
+		// Potentially useful code incase of rework:
+		//BattleTheme = Resources.Load<AudioClip>("Resources/Audio/Level Music/Battle_Theme_Final");
+		//FinalBattleTheme = Resources.Load<AudioClip>("Resources/Audio/Level Music/Final_Fight_Draft1");
+		
+		BGM = GameObject.Find("BackgroundMusic");
+		
+		if (!AudioBegin)
+		{
+			_musicManager.Play();
+			DontDestroyOnLoad(BGM);
+			AudioBegin = true;
+		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		_currentScene = SceneManager.GetActiveScene();
+		_currentSceneName = _currentScene.name;
+		
+		if (_currentSceneName == "Throne_Room" || _currentSceneName == "Rogue_Hideout" || _currentSceneName == "StartScreen")
+		{
+			_musicManager.Pause();
+		}
+
+		else
+		{
+			_musicManager.UnPause();
+		}
+	}
+}
+
