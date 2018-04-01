262b3a87b856a84a0776e6ebb22583fb72286e42
Evan Funnell <funnelle@mymacewan.ca>
2018-04-01 12:01:48 -0600

    Making sure knight changes uploaded

--- Assets/Scripts/Level/Bangin' Banners/PlatformBreaking.cs
+++ Assets/Scripts/Level/Bangin' Banners/PlatformBreaking.cs
@@ -3,23 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformBreaking : MonoBehaviour {
-	private BoxCollider2D leftLeg;
-	private BoxCollider2D rightLeg;
+	public GameObject leftLeg;
+	public GameObject rightLeg;
 	private Rigidbody2D rb2d;
 
 	// Use this for initialization
 	void Awake () {
 		rb2d = GetComponent<Rigidbody2D> ();
-		leftLeg = GetComponentInChildren<BoxCollider2D> ();
-		rightLeg = GetComponentInChildren<BoxCollider2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if ((leftLeg.isActiveAndEnabled == false) && (rightLeg.isActiveAndEnabled == false)) {
+		if ((leftLeg.activeSelf == false) && (rightLeg.activeSelf == false)) {
 			//play destruction animation
-			Debug.Log("Platform Broke!");
 			rb2d.bodyType = RigidbodyType2D.Dynamic;
+			PlatformEffector2D effector = GetComponent<PlatformEffector2D> ();
+			effector.colliderMask ^= (1 << 13);
 		}
 	}
 }
